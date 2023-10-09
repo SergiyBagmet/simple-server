9c9fdef0fefb0fe2004cfc92dd999fb99cd650db
SergiyBagmet <134723713+SergiyBagmet@users.noreply.github.com>
2023-10-10 00:49:32 +0300

    send html+static

--- application/app.py
+++ application/app.py
@@ -1,25 +1,48 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import pathlib
 import urllib.parse
+import mimetypes
 
+
+BASE_DIR = pathlib.Path()
 
 class HttpHandler(BaseHTTPRequestHandler):
+    
+    
     def do_GET(self):
-        pr_url = urllib.parse.urlparse(self.path)
-        match pr_url:
+        route = urllib.parse.urlparse(self.path)
+        match route.path:
             case '/':
                 self.send_html_file('index.html')
-            case'/contact':
-                self.send_html_file('contact.html')
+            case'/message':
+                self.send_html_file('message.html')
             case _:
-                self.send_html_file('error.html', 404)
+                file = BASE_DIR / route.path[1:]
+                if file.exists():
+                    self.send_static(file)
+                else:    
+                    self.send_html_file('error.html', 404)
 
-    def send_html_file(self, filename, status=200):
-        self.send_response(status)
+
+    def send_html_file(self, filename, status_code=200):
+        self.send_response(status_code)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
         
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
+            
+            
+    def send_static(self, filename):
+        self.send_response(200)
+        mime_tipe, *_ = mimetypes.guess_type(filename)
+        mime_tipe = mime_tipe if mime_tipe else 'text/plain'
+        
+        self.send_header('Content-type', mime_tipe)  
+        self.end_headers()
+        
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())        
 
 
 def run(server_class=HTTPServer, handler_class=HttpHandler):
